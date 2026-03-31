20365f07cfb41e988cfedd746899918555af3b99
boryano04 <boryano04@gmail.com>
2026-03-31 22:01:36 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -8,29 +8,45 @@ def get_assignment():
     conn = sqlite3.connect("museum.db")
     cursor = conn.cursor()
 
-    # задание 10
-    query_10 = """
-    SELECT t.name, COUNT(w.id)
+    query_10_1 = """
+    SELECT *
+    FROM types
+    WHERE name LIKE '%изделие%';
+    """
+    cursor.execute(query_10_1)
+    res10_1 = cursor.fetchall()
+
+    query_10_2 = """
+    SELECT 
+        t.id AS type_id,
+        t.name AS type_name,
+        COUNT(w.id) AS items_count
     FROM types t
     LEFT JOIN wings w ON w.type_id = t.id
-    GROUP BY t.id;
+    GROUP BY t.id, t.name
+    ORDER BY items_count DESC, t.name;
     """
-    cursor.execute(query_10)
-    res10 = cursor.fetchall()
+    cursor.execute(query_10_2)
+    res10_2 = cursor.fetchall()
 
-    # задание 11
     query_11 = """
     SELECT 
-        t.name,
+        t.id AS type_id,
+        t.name AS type_name,
+        ROUND(SUM(m.price * w.profit * p.scale), 2) AS total_revenue,
+        ROUND(SUM(m.price), 2) AS marketing_cost,
+        ROUND(AVG(w.profit), 2) AS avg_profit,
         ROUND(
-            (SUM(m.price * w.profit * p.scale) - SUM(m.price)) 
-            / SUM(m.price) * 100, 2
-        ) as roi
+            ((SUM(m.price * w.profit * p.scale) - SUM(m.price)) / SUM(m.price)) * 100,
+            2
+        ) AS roi_percent
     FROM moves m
     JOIN wings w ON m.wing_id = w.id
     JOIN types t ON w.type_id = t.id
     JOIN places p ON m.place_id = p.id
-    GROUP BY t.id;
+    GROUP BY t.id, t.name
+    HAVING SUM(m.price) > 0
+    ORDER BY roi_percent DESC, total_revenue DESC;
     """
     cursor.execute(query_11)
     res11 = cursor.fetchall()
@@ -38,6 +54,7 @@ def get_assignment():
     conn.close()
 
     return {
-        "task10": res10,
+        "task10_1": res10_1,
+        "task10_2": res10_2,
         "task11": res11
     }
